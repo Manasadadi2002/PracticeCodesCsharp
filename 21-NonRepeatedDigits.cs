016856ce5bddd0161ca367df6d354b03165c9ad6
Manasadadi2002 <115692768+Manasadadi2002@users.noreply.github.com>
2023-05-11 14:43:35 +0530

    Add files via upload

---
+++ 21-NonRepeatedDigits.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_tasks
+{
+    class _21_NonRepeatedDigits
+    {
+        public static void Main()
+        {
+            int[] array = new int[5];
+            int j, k;
+            Console.WriteLine("Enter the digits");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+
+
+            }
+            for (j = 0; j < array.Length; j++)
+            {
+                for (k = 0; k < array.Length; k++)
+                {
+                    if (array[j] == array[k] && (j != k))
+                        break;
+                }
+                if (k == array.Length)
+                    Console.WriteLine("Non Repested Digits are" + array[j]);
+
+
+
+            }
+        }
+    }
+}
